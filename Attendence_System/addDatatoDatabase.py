02e36a55d9e190fecef71bf7a362f5b30b305727
Abhishek Ghosh <abhishekp492002@gmail.com>
2023-09-13 23:45:51 +0530

    Updated

---
+++ Attendence_System/addDatatoDatabase.py
@@ -0,0 +1,32 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+cred = credentials.Certificate("Attendence_System\serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL':"https://medical-appointment-scheduler-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
+
+
+ref = db.reference('Doctors')
+
+data = {
+    "1231":
+    {
+        'NAME': "Dr Abhishek Ghosh",
+        'DEPT': "CARDIOLOGY",
+        'YEARS': "14"
+    },
+
+    "1232":
+    {
+        'NAME': "Dr Ankit Bhattacharya",
+        'DEPT': "ONCOLOGIST",
+        'YEARS': "15"
+    }
+}
+
+
+for key,value in data.items():
+    ref.child(key).set(value)
--- Attendence_System/main.py
+++ Attendence_System/main.py
@@ -6,17 +6,18 @@ from datetime import datetime
 
 
 
-path = 'Attendence_System\Training images'
+path = 'Attendence_System/Training images'
 images = []
 classNames = []
 alredy_attended = []
 myList = os.listdir(path)
-print(myList)                    
+print(myList)
 for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
+
 
 def findEncodings(images):
     encodeList = []
@@ -26,12 +27,20 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
+
 def video_overlay(bgimg,img,location):
     img=cv2.resize(img,(281,455)) 
     # h,w=img1.shape[:2]
     h1,w1=img.shape[:2]
     x,y=location
     bgimg[y:y+h1,x:x+w1]=img
+    return bgimg
+
+def image_overlay(bgimg,img2,location2):
+    # img2=img2.resize(img2,(315,355)) 
+    h1,w1=img2.shape[:2]
+    x,y=location2
+    bgimg[y:y+h1,x:x+w1]=img2
     return bgimg
 
 def markAttendance(name,alredy_attended):
@@ -43,8 +52,6 @@ def markAttendance(name,alredy_attended):
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
-
-
             if name not in nameList and name not in alredy_attended:
                 print("Writing name: Dr",name)
                 now = datetime.now()
@@ -55,21 +62,35 @@ def markAttendance(name,alredy_attended):
                 print(("ALREADY MARKED"))
                 break
 
-
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
-bgimg=cv2.imread('RESOURCES\CARDS\BACKGROUND.jpg')
+bgimg=cv2.imread('RESOURCES\BACKGROUND.jpg')
 print("dimensions=" , bgimg.shape)
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+folderModepath = 'RESOURCES\CARDS'
+modePathList = os.listdir(folderModepath)
+
+imgModeList=[]
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModepath,path)))
+# print(len(imgModeList))
+
+
+
+
+
+
 
 while True:
     success, img = cap.read()
+    img2=imgModeList[0]
     merge=video_overlay(bgimg,img,location=(37,80)) 
+    merge=image_overlay(bgimg,img2,location2=(351,129)) 
     cv2.namedWindow("Merge", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("Merge", 700, 800)
     # imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
@@ -97,30 +118,45 @@ while True:
 
 # /////////////////////////////////////////////////////////////////////
 
-# def video_overlay(bgimg,img,location):
-#     # img2=cv2.resize(img2,(500,500)) 
+# def video_overlay(bgimg,img1,location1):
+#     img1=cv2.resize(img1,(281,455)) 
 #     # h,w=img1.shape[:2]
-#     h1,w1=img.shape[:2]
-#     x,y=location
-#     bgimg[y:y+h1,x:x+w1]=img
+#     h1,w1=img1.shape[:2]
+#     x,y=location1
+#     bgimg[y:y+h1,x:x+w1]=img1
 #     return bgimg
 
-# bgimg=cv2.imread('RESOURCES\CARDS\BACKGROUND.jpg')
+# def image_overlay(bgimg,img2,location2):
+#     # img2=img2.resize(img2,(315,355)) 
+#     h1,w1=img2.shape[:2]
+#     x,y=location2
+#     bgimg[y:y+h1,x:x+w1]=img2
+#     return bgimg
+
+# bgimg=cv2.imread('RESOURCES\BACKGROUND.jpg')
 # cap=cv2.VideoCapture(0)
-# bgimg = cv2.cvtColor(bgimg, cv2.COLOR_BGR2RGB)
-# # img1 = cv2.resize(img1, (1000, 1000))
+
 # print("dimensions=" , bgimg.shape)
 
 # cap.set(3, 640)
 # cap.set(4, 480)
 
 
+# folderModepath = 'RESOURCES\CARDS'
+# modePathList = os.listdir(folderModepath)
+
+# imgModeList=[]
+# for path in modePathList:
+#     imgModeList.append(cv2.imread(os.path.join(folderModepath,path)))
+# # print(len(imgModeList))
+
+
+
 # while True:
-#     success,img=cap.read()
-#     print("dimensions=" , img.shape)
-#     # co-ordinates of webcam
-#     merge=video_overlay(bgimg,img,location=(100,100)) 
-#     # to resize the window
+#     success,img1=cap.read()
+#     img2=imgModeList[0]
+#     merge=video_overlay(bgimg,img1,location1=(37,80)) 
+#     merge=image_overlay(bgimg,img2,location2=(351,129)) 
 #     cv2.namedWindow("Merge", cv2.WINDOW_NORMAL)
 #     cv2.resizeWindow("Merge", 700, 800)
 #     cv2.imshow("Merge",merge)

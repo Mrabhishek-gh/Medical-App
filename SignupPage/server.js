6e5efa983611e94e4fa1821ee7ddc15d7fc8f336
Abhishek Ghosh <abhishekp492002@gmail.com>
2024-07-03 22:33:54 +0530

    commit

---
+++ SignupPage/server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const port = 3000
+const bodyParser = require('body-parser')
+
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://abhishekp492002:guddup49@login.xtuioku.mongodb.net/")
+
+const notesSchema={
+    
+    name: String,
+    email: String,
+    password : String
+
+}
+
+const Note = mongoose.model("Note", notesSchema);
+
+
+app.use(express.static(__dirname))
+
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'signup.html'))
+})
+
+
+app.post("/", function (req, res) {
+
+    let newNote = new Note({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    newNote.save();
+    res.redirect("/");
+    
+})
+
+app.listen(port,()=>{
+    console.log("Server started")
+})

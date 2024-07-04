0b6c607e3856ee9767b8a150538d867ce67bbb40
Abhishek Ghosh <abhishekp492002@gmail.com>
2024-07-04 23:02:46 +0530

    commit

---
+++ HOME.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const port = 3002
+const bodyParser = require('body-parser')
+
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://abhishekp492002:guddup49@login.xtuioku.mongodb.net/sample_mflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+
+const dataSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    number: String,
+    password: String,
+    add: String,
+    sym: String
+}, { collection: 'USERS' });
+
+const Note = mongoose.model("Note", dataSchema);
+
+
+app.use(express.static(__dirname))
+
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'HOME.html'))
+})
+
+
+app.post("/", function (req, res) {
+
+    let newNote = new Note({
+        name: req.body.name,
+        email: req.body.email,
+        number: req.body.number,
+        password:req.body.password,
+        add:req.body.add,
+        sym:req.body.sym
+    })
+    newNote.save();
+    res.redirect("/");
+    
+})
+
+app.listen(port,()=>{
+    console.log("Server started")
+})
